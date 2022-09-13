3001f2318f77c626bc66d73bdd1d95b9e31a458f
sjables <99538726+sjables@users.noreply.github.com>
2022-09-13 18:07:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -333,9 +333,9 @@ elif page == '📜 Upload and convert':
 
     tessdata_dir_config = r'--tessdata-dir "s3://saracapstone/tesseract/tessdata/"'
     
-    lang1 = r'-l ara'
-    lang2 = r'-l ara+ckbLayer+sarchia'
-    lang3 = r'-l ara+ckbLayer'
+    lang1 = r'ara'
+    lang2 = r'ara+ckbLayer+sarchia'
+    lang3 = r'ara+ckbLayer'
 
     persian = st.checkbox('Persian')
     arabic = st.checkbox('Arabic')
