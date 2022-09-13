e3a7be833a5096b452b1d5fa30a89a4abd9f0179
sjables <99538726+sjables@users.noreply.github.com>
2022-09-13 18:45:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -331,7 +331,7 @@ elif page == '📜 Upload and convert':
     #home_dir = Path.home()
     #tess_path = Path('/Users/sarajabbar/Desktop/trainingtesseract/tesseract/tessdata')
     pytesseract.pytesseract.tesseract_cmd = r'https://saracapstone.s3.amazonaws.com/tesseract/tesseract.exe'
-    tessdata_dir_config = r'--tessdata-dir "https://saracapstone.s3.amazonaws.com/tesseract/tessdata/"'
+    tessdata_dir_config = r'--tessdata-dir "s3://saracapstone/tesseract/tessdata"'
     
     lang1 = r'ara'
     lang2 = r'ara+ckbLayer+sarchia'
